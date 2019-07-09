d623a1ccfc48852549e82f7181f74f553bd5bd78
nidichaoge <1379202076@qq.com>
2019-07-09 19:34:56 +0800

    commit

--- bms-demo-config/src/main/java/com/mouse/bms/demo/config/BmsDemoConfigApplication.java
+++ bms-demo-config/src/main/java/com/mouse/bms/demo/config/BmsDemoConfigApplication.java
@@ -8,6 +8,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class BmsDemoConfigApplication {
 
+    /**
+     *                              /名字           /环境/分支
+     * 访问地址 http://127.0.0.1:8000/bms-demo-test6/test/master
+     * 从git获取到信息后，会存储在本地的应用文件系统中
+     */
     public static void main(String[] args) {
         SpringApplication.run(BmsDemoConfigApplication.class, args);
     }
