01fef09bfa19c44528bd2a90e5f9d72229e3c073
nidichaoge <1379202076@qq.com>
2019-07-09 13:19:51 +0800

    config client

--- bms-demo-config/src/main/java/com/mouse/bms/demo/config/BmsDemoConfigApplication.java
+++ bms-demo-config/src/main/java/com/mouse/bms/demo/config/BmsDemoConfigApplication.java
@@ -8,6 +8,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class BmsDemoConfigApplication {
 
+    /**
+     *                              /     /环境 /分支
+     * 访问地址 http://127.0.0.1:8000/mouse/prod/master
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(BmsDemoConfigApplication.class, args);
     }
