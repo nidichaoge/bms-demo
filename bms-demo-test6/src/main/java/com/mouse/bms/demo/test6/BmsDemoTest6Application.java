986d2c973806923f2ccb530688b82ef5feeb6735
nidichaoge <1379202076@qq.com>
2019-07-09 19:17:30 +0800

    config server test

---
+++ bms-demo-test6/src/main/java/com/mouse/bms/demo/test6/BmsDemoTest6Application.java
@@ -0,0 +1,13 @@
+package com.mouse.bms.demo.test6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsDemoTest6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsDemoTest6Application.class, args);
+    }
+
+}
